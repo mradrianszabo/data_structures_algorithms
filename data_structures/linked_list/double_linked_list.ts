5caa142e65b13746e0817ac17ef158d3c9db3aaf
mradrianszabo <mradrianszabo@gmail.com>
2021-12-03 19:43:31 +0100

    doubleLinkedList class added

--- data_structures/linked_list/double_linked_list.ts
+++ data_structures/linked_list/double_linked_list.ts
@@ -1,7 +1,14 @@
 class Node2<T>{
     constructor(
         public value : T,
-        public prev : Node2<T>,
-        public next : Node2<T>
+        public prev?: Node2<T>,
+        public next?: Node2<T>
     ){}
    }
+
+class DoubleLinkedList<T>{
+    constructor(
+        public head?: Node2<T>,
+        public tail?: Node2<T>
+    ){}
+}
