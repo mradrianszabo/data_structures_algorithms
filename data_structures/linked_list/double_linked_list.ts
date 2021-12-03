9f4d5e37011b568d83f6993d4109b200ed85964f
mradrianszabo <mradrianszabo@gmail.com>
2021-12-03 20:47:22 +0100

    try out implemented, delete, search fixed

--- data_structures/linked_list/double_linked_list.ts
+++ data_structures/linked_list/double_linked_list.ts
@@ -81,7 +81,7 @@ class DoubleLinkedList<T>{
             return oldTail;
         }
         let currentNode = this.head;
-        while(currentNode.value !== value || currentNode !== this.tail){
+        while(currentNode.next !== null){
             if(currentNode.value === value){
                 currentNode.prev.next = currentNode.next;
                 currentNode.next.prev = currentNode.prev;
@@ -99,8 +99,11 @@ class DoubleLinkedList<T>{
         if(this.head === this.tail){
             return this.head;
         }
+        if(this.tail.value === value){
+            return this.tail;
+        }
         let currentNode = this.head;
-        while(currentNode.value !== value || currentNode !== this.tail){
+        while(currentNode.next !== null){
             if(currentNode.value === value){
                 return currentNode;
             }
@@ -108,4 +111,40 @@ class DoubleLinkedList<T>{
         }
         return null;
     }
+}
+
+//try out
+//to run this code, run in terminal:
+//tsc double_linked_list.ts
+//node double_linked_list.js
+
+let doubleList = new DoubleLinkedList<string>();
+//append
+for(let elem of ['three', 'four', 'five', 'six', 'seven']){
+    doubleList.append(elem);
+    console.log(`${elem} appended. NEW HEAD:`, doubleList.head, 'NEW TAIL:', doubleList.tail);
+}
+//prepend
+for(let elem of ['two', 'one']){
+    doubleList.prepend(elem);
+    console.log(`${elem} prepended. NEW HEAD:`, doubleList.head, 'NEW TAIL: ', doubleList.tail);
+}
+//search
+let searched = doubleList.search('three');
+console.log('three searched. RESULT: ', searched);
+
+//deleteTail
+{
+    let deleted = doubleList.deleteTail();
+    console.log('Tail deleted. NEW TAIL: ', doubleList.tail, ' Deleted node: ', deleted);
+}
+//deleteHead
+{
+    let deleted = doubleList.deleteHead();
+    console.log('head deleted. NEW HEAD: ', doubleList.head, ' Deleted node: ', deleted);
+}
+//delete
+for(let elem of ['one', 'three', 'six']){
+    let deleted = doubleList.delete(elem);
+    console.log(elem, 'deleted. NEW HEAD: ', doubleList.head, 'NEW TAIL: ', doubleList.tail, ' Deleted node: ', deleted);
 }
