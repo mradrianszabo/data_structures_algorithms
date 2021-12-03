0126dd7a82433b886a97babec2a10bac7f3eebd1
mradrianszabo <mradrianszabo@gmail.com>
2021-12-03 23:08:17 +0100

    class -LinkedList- added

--- data_structures/hash_table/hash_table_linked_list.ts
+++ data_structures/hash_table/hash_table_linked_list.ts
@@ -2,10 +2,18 @@ class Node<T>{
     constructor(
         public key: string,
         public value: T,
-        public next: Node<T>,
-        public prev: Node<T>
+        public next?: Node<T>,
+        public prev?: Node<T>
     ){}
 }
+
+class LinkedList<T>{
+    constructor(
+        public head?: Node<T>,
+        public tail?: Node<T>,
+    ){}
+}
+
 export class HashTable<T>{
 
 }
