2c7d786b7ce26745b7e934f74f0e5b2bea6b6505
mradrianszabo <mradrianszabo@gmail.com>
2021-12-03 16:30:50 +0100

    method -search- fixed

--- data_structures/linked_list/single_linked_list.js
+++ data_structures/linked_list/single_linked_list.js
@@ -2,116 +2,112 @@
 //Time complexity: Access : O(n); Search : O(n) Insertion : O(1) Deletion : O(n);
 //Advantage : add and remove way faster than at array
 //Disadvantage: no direct access(has only sequential access) like array has.
-
-class NodeElement<T>{
-    constructor(
-        public value : T,
-        public next?: NodeElement<T>,
-    ){}
-}
-
-class SingleLinkedList<T>{
-    constructor(
-        public head?: NodeElement<T>,
-        public tail?: NodeElement<T>,
-    ){}
-
-    public append(value : T) : void{
-        let newNode = new NodeElement<T>(value)
-        if(!this.tail){
+var NodeElement = /** @class */ (function () {
+    function NodeElement(value, next) {
+        this.value = value;
+        this.next = next;
+    }
+    return NodeElement;
+}());
+var SingleLinkedList = /** @class */ (function () {
+    function SingleLinkedList(head, tail) {
+        this.head = head;
+        this.tail = tail;
+    }
+    SingleLinkedList.prototype.append = function (value) {
+        var newNode = new NodeElement(value);
+        if (!this.tail) {
             this.head = this.tail = newNode;
-        }else{
-            let oldTail = this.tail;
+        }
+        else {
+            var oldTail = this.tail;
             this.tail = newNode;
             oldTail.next = this.tail;
         }
-    }
-
-    public prepend(value : T) : void{
-        let newNode = new NodeElement<T>(value);
-        if(!this.head){
+    };
+    SingleLinkedList.prototype.prepend = function (value) {
+        var newNode = new NodeElement(value);
+        if (!this.head) {
             this.head = this.tail = newNode;
-        }else{
-            let oldHead = this.head;
+        }
+        else {
+            var oldHead = this.head;
             this.head = newNode;
             this.head.next = oldHead;
         }
-    }
-
-    public deleteHead() : T{
-        if(!this.head){
+    };
+    SingleLinkedList.prototype.deleteHead = function () {
+        if (!this.head) {
             return null;
         }
-        let oldHead = this.head;
-        if(this.head === this.tail){
+        var oldHead = this.head;
+        if (this.head === this.tail) {
             this.head = this.tail = null;
-        }else{
+        }
+        else {
             this.head = this.head.next;
         }
         return oldHead.value;
-    }
-
-    public deleteTail() : T{
-        if(!this.tail){
+    };
+    SingleLinkedList.prototype.deleteTail = function () {
+        if (!this.tail) {
             return null;
         }
-        let oldTail = this.tail;
-        if(this.head === this.tail){
+        var oldTail = this.tail;
+        if (this.head === this.tail) {
             this.head = this.tail = null;
-        }else{
-            let currentNode = this.head;
-            while(currentNode.next.next !== null){
+        }
+        else {
+            var currentNode = this.head;
+            while (currentNode.next.next !== null) {
                 currentNode = currentNode.next;
             }
             currentNode.next = null;
             this.tail = currentNode;
         }
         return oldTail.value;
-    }
-
-    public delete(value : T) : T{
-        if(!this.head){
+    };
+    SingleLinkedList.prototype["delete"] = function (value) {
+        if (!this.head) {
             return null;
         }
-        if(this.head.value === value){
-            let deleted = this.head;
+        if (this.head.value === value) {
+            var deleted_1 = this.head;
             this.head = this.head.next;
-            return deleted.value;
+            return deleted_1.value;
         }
-        let currentNode = this.head;
-        while(currentNode.next !== null || currentNode.next.value !== value){
+        var currentNode = this.head;
+        while (currentNode.next !== null || currentNode.next.value !== value) {
             currentNode = currentNode.next;
         }
-        if(currentNode.next.value === value){
-            let deleted = currentNode.next;
+        if (currentNode.next.value === value) {
+            var deleted_2 = currentNode.next;
             currentNode.next = currentNode.next.next;
-            return deleted.value;
+            return deleted_2.value;
         }
         return null;
-    }
-
-    public search(value : T) : NodeElement<T>{
-        if(!this.head){
+    };
+    SingleLinkedList.prototype.search = function (value) {
+        if (!this.head) {
             return null;
         }
-        if(this.head === this.tail){
+        if (this.head === this.tail) {
             return this.head;
         }
-        let currentNode = this.head;
-        while(currentNode.next !== null){
-            if(currentNode.value === value){
+        var currentNode = this.head;
+        while (currentNode.next !== null) {
+            console.log('CURRENT:', currentNode.value);
+            if (currentNode.value === value) {
                 return currentNode;
             }
             currentNode = currentNode.next;
         }
         return null;
-    }
-}
-
+    };
+    return SingleLinkedList;
+}());
 //try out
-
-let listOfNumbers = new SingleLinkedList<number>();
-
+var listOfNumbers = new SingleLinkedList();
 listOfNumbers.append(2);
 console.log('2 appended to empty list. HEAD: ', listOfNumbers.head.value, 'TAIL: ', listOfNumbers.tail.value);
 listOfNumbers.append(3);
@@ -123,21 +119,18 @@ listOfNumbers.append(5);
 listOfNumbers.append(6);
 listOfNumbers.append(7);
 console.log('4, 5, 6, 7 appended to list. HEAD: ', listOfNumbers.head.value, 'TAIL: ', listOfNumbers.tail.value);
-
-let searched = listOfNumbers.search(1);
+var searched = listOfNumbers.search(1);
 console.log('search for 1. RESULT: ', searched);
 searched = listOfNumbers.search(7);
 console.log('search for 7. RESULT: ', searched);
 searched = listOfNumbers.search(3);
 console.log('searched for 3. RESULT: ', searched);
-
-let deleted = listOfNumbers.delete(1);
+var deleted = listOfNumbers["delete"](1);
 console.log('1 deleted. HEAD: ', listOfNumbers.head.value, 'deleted elem value: ', deleted);
-deleted = listOfNumbers.delete(7);
+deleted = listOfNumbers["delete"](7);
 console.log('7 deleted. TAIL: ', listOfNumbers.tail.value, 'deleted elem value: ', deleted);
-deleted = listOfNumbers.delete(3);
+deleted = listOfNumbers["delete"](3);
 console.log('3 deleted. ', 'deleted elem value: ', deleted);
-
 deleted = listOfNumbers.deleteHead();
 console.log('head deleted. HEAD: ', listOfNumbers.head.value, 'deleted elem value: ', deleted);
 deleted = listOfNumbers.deleteTail();
