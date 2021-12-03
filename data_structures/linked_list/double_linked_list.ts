4326b7db10e9b94ccb9be0aecb90fe66699e1ce7
mradrianszabo <mradrianszabo@gmail.com>
2021-12-03 19:46:17 +0100

    append added

--- data_structures/linked_list/double_linked_list.ts
+++ data_structures/linked_list/double_linked_list.ts
@@ -11,4 +11,16 @@ class DoubleLinkedList<T>{
         public head?: Node2<T>,
         public tail?: Node2<T>
     ){}
+
+    public append(value: T) : void{
+        let newNode = new Node2<T>(value);
+        if(!this.head){
+            this.head = this.tail = newNode;
+        }else{
+            let oldTail = this.tail;
+            this.tail = newNode;
+            oldTail.next = this.tail;
+            this.tail.prev = oldTail;
+        }
+    }
 }
