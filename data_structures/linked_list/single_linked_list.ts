3190d6ea70e084930ca68f5d0c82cdfe27c8fd18
mradrianszabo <mradrianszabo@gmail.com>
2021-12-03 14:49:35 +0100

    introduction added

---
+++ data_structures/linked_list/single_linked_list.ts
@@ -0,0 +1,4 @@
+//A linear collection of data elements. Each element points to the next one. Each node(element) is composed of data and a reference(link) to the next node.
+//Time complexity: Access : O(n); Search : O(n) Insertion : O(1) Deletion : O(n);
+//Advantage : add and remove way faster than at array
+//Disadvantage: no direct access(has only sequential access) like array has.
