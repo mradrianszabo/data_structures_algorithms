7e22dc6d48beb15c4655e09b312e2bd6e9ac0cac
mradrianszabo <mradrianszabo@gmail.com>
2021-12-03 16:05:00 +0100

    testcase of append, prepend, search, delete added. Delete method req. fixes

--- data_structures/linked_list/single_linked_list.ts
+++ data_structures/linked_list/single_linked_list.ts
@@ -12,8 +12,8 @@ class NodeElement<T>{
 
 class SingleLinkedList<T>{
     constructor(
-        public head : NodeElement<T>,
-        public tail : NodeElement<T>,
+        public head?: NodeElement<T>,
+        public tail?: NodeElement<T>,
     ){}
 
     public append(value : T) : void{
@@ -103,4 +103,34 @@ class SingleLinkedList<T>{
         return null;
     }
 }
+
+//try out
+
+let listOfNumbers = new SingleLinkedList<number>();
+
+listOfNumbers.append(2);
+console.log('2 appended to empty list. HEAD: ', listOfNumbers.head, 'TAIL: ', listOfNumbers.tail);
+listOfNumbers.append(3);
+console.log('3 appended to list. HEAD: ', listOfNumbers.head, 'TAIL: ', listOfNumbers.tail);
+listOfNumbers.prepend(1);
+console.log('1 prepended to list. HEAD: ', listOfNumbers.head, 'TAIL: ', listOfNumbers.tail);
+listOfNumbers.append(4);
+listOfNumbers.append(5);
+listOfNumbers.append(6);
+listOfNumbers.append(7);
+console.log('4, 5, 6, 7 appended to list. HEAD: ', listOfNumbers.head, 'TAIL: ', listOfNumbers.tail);
+
+let searched = listOfNumbers.search(1);
+console.log('search for 1. RESULT: ', searched);
+searched = listOfNumbers.search(7);
+console.log('search for 7. RESULT: ', searched);
+searched = listOfNumbers.search(3);
+console.log('searched for 3. RESULT: ', searched);
+
+let deleted = listOfNumbers.delete(1);
+console.log('1 deleted. HEAD: ', listOfNumbers.head, 'deleted elem value: ', deleted);
+deleted = listOfNumbers.delete(7);
+console.log('7 deleted. TAIL: ', listOfNumbers.tail, 'deleted elem value: ', deleted);
+deleted = listOfNumbers.delete(3);
+console.log('3 deleted. ', 'deleted elem value: ', deleted);
 
