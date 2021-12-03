bb1815317b33c602cc3aa0e4b82b56fb95ab5604
mradrianszabo <mradrianszabo@gmail.com>
2021-12-03 22:04:24 +0100

    method -createTable- added, and called in constructor

--- data_structures/hash_table/hash_table_map.ts
+++ data_structures/hash_table/hash_table_map.ts
@@ -6,6 +6,15 @@ export class HashTable<T>{
         public size: number = 3,
         public autoResize: boolean = true,
     ){
-        
+        this.table = this.createTable();
+        this.numberOfItems = 0;
+    }
+
+    private createTable() : Map<string, T>[]{
+        let table = Array(this.size);
+        for(let i = 0; i<this.size; i++){
+            table[i] = new Map<string, T>();
+        }
+        return table;
     }
 }
